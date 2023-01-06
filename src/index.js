fa5f4190daa094585de34def5afa0efc3f8b499f
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2023-01-06 02:11:13 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ client.connect().then(() => {
 	console.log("Connected");
 	client.on("PM", (message) => console.log(`${message.user.ircUsername}: ${message.message}`));
 }).catch(console.error);
-// uses Bancho.js
+// uses Bancho.js, use npm i bancho.js and paste your inputs  in unless you want to modify this
